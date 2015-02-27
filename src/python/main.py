a7607b7db34917e62412822a38a51f0d6386e441
titema <marion@emersion.fr>
2015-02-27 17:59:11 +0100

    Create main.py

---
+++ src/python/main.py
@@ -0,0 +1,76 @@
+#-*- coding: utf-8 -*-
+
+# Importer la librairie sqlite3
+import sqlite3
+import urllib2
+import xml.dom.minidom
+
+def getText(nodeList):
+    rc = []
+    for node in nodeList:
+        if node.nodeType == node.TEXT_NODE:
+            rc.append(node.data)
+    return ''.join(rc)
+
+# Connexion à la base de donnée
+base = sqlite3.connect('librecast.db')
+
+# On crée un curseur (qui va agir sur la base)
+curseur = base.cursor()
+
+# On crée la table "feeds"
+#curseur.execute('CREATE TABLE feeds (id INTEGER PRIMARY KEY AUTOINCREMENT, url text)')
+
+# On demande une adresse de flux
+adresse = raw_input("Une adresse siouplé monsieur : ")
+
+if (adresse != ""):
+    # On compte le nombre d'élément qui ont pour url la variable adresse
+    curseur.execute('SELECT COUNT(*) FROM feeds WHERE url = :adresse',{"adresse":adresse})
+    resultat = curseur.fetchone()[0]
+
+    if (resultat > 0):
+        print 'Cette adresse existe déjà'
+    else:
+        print 'Cette adresse est nouvelle, je vais l\'ajouter !'
+        curseur.execute('INSERT INTO feeds (url) VALUES (:adresse)',{"adresse":adresse})
+    
+# On sépare !
+print '-----------------------------'
+
+# On demande tous les feeds
+curseur.execute('SELECT * FROM feeds')
+
+# On récupère tous les résultats
+resultats = curseur.fetchall()
+
+# On les affiche
+for resultat in resultats:
+    print resultat[1]
+
+if (raw_input("Voulez-vous supprimer une URL (oui/NON) ? ") == "oui"):
+    adresse = raw_input("Quelle adresse : ")
+    curseur.execute('DELETE FROM feeds WHERE url = :adresse',{'adresse':adresse})
+
+# On sépare !
+print '-----------------------------'
+
+# On demande tous les feeds
+curseur.execute('SELECT * FROM feeds')
+
+# On récupère tous les résultats
+resultats = curseur.fetchall()
+
+# On affiche le fichier situé à chaque URL
+for resultat in resultats:
+    resultat_de_la_requete = urllib2.urlopen(resultat[1])
+    flux = resultat_de_la_requete.read()
+    flux_parser = xml.dom.minidom.parseString(flux)
+    print getText(flux_parser.getElementsByTagName("title")[0].childNodes)
+
+
+# On enregistre les modifications
+base.commit()
+
+# On ferme la base
+base.close()
