358e2d4500144c1bce8f63469fcb5ad9c52769d4
TiBounise <contact@tibounise.com>
2015-05-14 21:40:19 +0200

    Trimming the fat on the download window (again)

--- src/python/uiManager/downloadManager.py
+++ src/python/uiManager/downloadManager.py
@@ -1,12 +1,20 @@
 # -*- coding: utf-8 -*-
 
-import requests
 import os
 import sys
 import wx
 import wx.lib.scrolledpanel as scrolled
 
-from wx.lib.pubsub import pub
+try:
+    approot = os.path.dirname(os.path.abspath(__file__))
+except NameError:  # We are the main py2exe script, not a module
+    approot = os.path.dirname(os.path.abspath(sys.argv[0]))
+
+if('.exe' in approot):
+    approot = approot.replace('LibreCast.exe', '')
+
+if('uiManager' in approot):
+    approot = approot.replace('/uiManager', '')
 
 class Gauge(wx.Gauge):
     def __init__(self, parent, range, num):
@@ -33,62 +41,44 @@ class DownloadPanel(scrolled.ScrolledPanel):
         self.SetSizer(self.mainSizer)
         self.SetAutoLayout(1)
         self.SetupScrolling()
-        self.onDownload(None)
-        self.onDownload(None)
+        self.addDownload(None)
+        self.addDownload(None)
 
-    def onDownload(self, event):
-        url = "http://tibounise.com"
+    def addDownload(self,url):
+    	panel = wx.Panel(self)
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        fname = "XViD.Screener.illegal.avi"
+        lbl = wx.StaticText(panel, label="%s" % fname)
+        self.infoLabel = wx.StaticText(panel, label="%s of %s (%s/sec) — %s remaining" % ("53.7", "195 MB", "21.6 MB", "6 seconds"))
+        font = wx.Font(10, wx.DEFAULT, wx.NORMAL, wx.NORMAL)
+        self.infoLabel.SetFont(font)
 
-        # Set root path
-        try:
-            approot = os.path.dirname(os.path.abspath(__file__))
-        except NameError:  # We are the main py2exe script, not a module
-            approot = os.path.dirname(os.path.abspath(sys.argv[0]))
+        progressSizer = wx.BoxSizer(wx.HORIZONTAL)
+        gauge = Gauge(panel, 8, self.download_number)
+        cancelImage = wx.Image(os.path.join(os.environ.get('RESOURCEPATH', approot), 'uiManager', 'resources', 'cancel.png'))
+        cancelImage.Rescale(15, 15)
+        cancelButton = wx.BitmapButton(panel, wx.ID_ANY, wx.BitmapFromImage(cancelImage), style=wx.NO_BORDER)
 
-        if('.exe' in approot):
-            approot = approot.replace('LibreCast.exe', '')
+        progressSizer.Add(gauge, 1, wx.RIGHT | wx.EXPAND, 10)
+        progressSizer.Add(cancelButton, 0, wx.RIGHT | wx.LEFT, 5)
 
-        if('uiManager' in approot):
-            approot = approot.replace('/uiManager', '')
+        sizer.Add(lbl, 0, wx.TOP | wx.RIGHT | wx.LEFT, 5)
+        sizer.Add(progressSizer, 1, wx.RIGHT | wx.LEFT | wx.EXPAND, 10)
+        sizer.Add(self.infoLabel, 0, wx.RIGHT | wx.LEFT | wx.BOTTOM, 5)
 
-        try:
-            panel = wx.Panel(self)
-            sizer = wx.BoxSizer(wx.VERTICAL)
-            fname = "XViD.Screener.illegal.avi"
-            lbl = wx.StaticText(panel, label="%s" % fname)
-            self.infoLabel = wx.StaticText(panel, label="%s of %s (%s/sec) — %s remaining" % ("53.7", "195 MB", "21.6 MB", "6 seconds"))
-            font = wx.Font(10, wx.DEFAULT, wx.NORMAL, wx.NORMAL)
-            self.infoLabel.SetFont(font)
+        if self.download_number % 2 != 0:
+            panel.SetBackgroundColour(wx.Colour(255, 255, 255))
+        else:
+            panel.SetBackgroundColour(wx.Colour(239, 245, 255))
+        panel.SetSizer(sizer)
+        self.mainSizer.Add(panel, 0, wx.EXPAND)
 
-            progressSizer = wx.BoxSizer(wx.HORIZONTAL)
-            gauge = Gauge(panel, 8, self.download_number)
-            cancelImage = wx.Image(os.path.join(os.environ.get('RESOURCEPATH', approot), 'uiManager', 'resources', 'cancel.png'))
-            cancelImage.Rescale(15, 15)
-            cancelButton = wx.BitmapButton(panel, wx.ID_ANY, wx.BitmapFromImage(cancelImage), style=wx.NO_BORDER)
+        self.Layout()
 
-            progressSizer.Add(gauge, 1, wx.RIGHT | wx.EXPAND, 10)
-            progressSizer.Add(cancelButton, 0, wx.RIGHT | wx.LEFT, 5)
-
-            sizer.Add(lbl, 0, wx.TOP | wx.RIGHT | wx.LEFT, 5)
-            sizer.Add(progressSizer, 1, wx.RIGHT | wx.LEFT | wx.EXPAND, 10)
-            sizer.Add(self.infoLabel, 0, wx.RIGHT | wx.LEFT | wx.BOTTOM, 5)
-
-            if self.download_number % 2 != 0:
-                panel.SetBackgroundColour(wx.Colour(255, 255, 255))
-            else:
-                panel.SetBackgroundColour(wx.Colour(239, 245, 255))
-            panel.SetSizer(sizer)
-            self.mainSizer.Add(panel, 0, wx.EXPAND)
-
-            self.Layout()
-
-            # start thread
-            self.download_number += 1
-        except Exception, e:
-            print "Error: ", e
+        # start thread
+        self.download_number += 1
 
         self.SetupScrolling()
-
 
 class DownloaderFrame(wx.Frame):
     def __init__(self):
