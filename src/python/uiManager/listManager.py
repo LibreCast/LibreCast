81a75fdc7374bc0d89bd7508372d1f7606c8b9b5
JRock007 <jrg@MacBook.local>
2015-05-16 17:05:05 +0200

    Streaming works with correct URL

--- src/python/uiManager/listManager.py
+++ src/python/uiManager/listManager.py
@@ -150,4 +150,4 @@ class pyList(wx.ListCtrl):
     def PlayVideo(self, event):
         print 'Lancer la vidéos à l\'index ' + str(event.GetIndex())
         # Afficher une fenêtre avec la vidéo située à l'URL donnée
-        videoManager.videoWindow(self, wx.ID_ANY, URL)
+        videoManager.videoWindow(self, wx.ID_ANY, self.URLsByIndex[event.GetIndex()][0])
