9dde39f8cee362ab309b7444f7f35d19d32545ba
JRock007 <JRock007@users.noreply.github.com>
2015-05-16 13:31:34 +0200

    Improved time converter and removed useless import in request manager

--- src/python/requestsManager/httpRequestManager.py
+++ src/python/requestsManager/httpRequestManager.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
 import urllib2
-import httplib
 
 # Liste de codes d'erreur qui pourraient suggérer qu'ajouter "/feed.xml" ou "/feed.rss" donne une URL valide
 handledCodes = ['404', '410', '415', '418', '500']
@@ -15,7 +14,7 @@ def OpenUrl(url):
     # Un bloc try/catch pour capturer les exceptions lors de la requête, s'il y en a
     try:
         # Télécharger le fichier situé à l'adresse indiquée
-        output = urllib2.urlopen(url,timeout=5)
+        output = urllib2.urlopen(url, timeout=5)
 
     # Si le téléchargement donne une erreur http (ex : 404)
     except urllib2.HTTPError, e:
